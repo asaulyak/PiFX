6f3a86310aa6caffb6013b2b3211315bf1308f5d
asaulyak <jenya.asaulyak@gmail.com>
2014-12-14 16:09:29 +0200

    Add some annimations

--- server.js
+++ server.js
@@ -1,52 +1,53 @@
-var http 		= require('http'),
+var http = require('http'),
 
-	handler		= require('./static'),
+	handler = require('./static'),
 
-	app			= http.createServer(handler),
-	io			= require('socket.io').listen(app),
+	app = http.createServer(handler),
+	io = require('socket.io').listen(app),
 
-	spi 		= require('spi'),
+	spi = require('spi'),
 
-	RPixel		= require('raspberrypixels'),
+	RPixel = require('raspberrypixels'),
 
 	AvailableAnimations = require('./animationloader').load()
-	Animations	= []
+Animations = [];
 
-var PIXELS  = parseInt(process.env.PIXELS, 10) || 25
-var DEVICE 	= process.env.DEVICE || '/dev/spidev0.0'
-var PORT 	= process.env.PORT || 8888
+var PIXELS = parseInt(process.env.PIXELS, 10) || 50;
+var DEVICE = process.env.DEVICE || '/dev/spidev0.0';
+var PORT = process.env.PORT || 8777;
 
-var FPS		= 30
-var RUNNING	= true
+var FPS = 30;
+var RUNNING = true;
 
-app.listen(parseInt(PORT, 10))
+app.listen(parseInt(PORT, 10));
 
 console.log("HTTP server listening on port " + PORT)
 
-var Device				= new spi.Spi(DEVICE, function(){});
-var Pixels 				= new RPixel.PixelBuffer(PIXELS)
-var ActiveAnimations 	= []
-var Frame 				= 0
-var ReadBuffer			= new Buffer(PIXELS * 3)
+var Device = new spi.Spi(DEVICE, function () {
+});
+var Pixels = new RPixel.PixelBuffer(PIXELS)
+var ActiveAnimations = []
+var Frame = 0
+var ReadBuffer = new Buffer(PIXELS * 3)
 
-for(var i = 0; i < AvailableAnimations.length; i++){
+for (var i = 0; i < AvailableAnimations.length; i++) {
 	Animations.push(new AvailableAnimations[i](PIXELS))
 }
 
-RenderStrip() // Begin the strip animation
+RenderStrip(); // Begin the strip animation
 
 io.sockets.on('connection', function (socket) {
 	socket.emit('initialize', {
 		animations: Strip(Animations),
 		activeAnimations: Strip(ActiveAnimations)
-	})
+	});
 
-	socket.on('animations', function(activeAnimations){
+	socket.on('animations', function (activeAnimations) {
 		ActiveAnimations = []
 
-		for(var i = 0; i < activeAnimations.length; i++){
-			for(var a = 0; a < Animations.length; a++){
-				if(activeAnimations[i].name == Animations[a].name){
+		for (var i = 0; i < activeAnimations.length; i++) {
+			for (var a = 0; a < Animations.length; a++) {
+				if (activeAnimations[i].name == Animations[a].name) {
 					var anim = new AvailableAnimations[a](PIXELS)
 					anim.config = activeAnimations[i].config;
 
@@ -58,7 +59,7 @@ io.sockets.on('connection', function (socket) {
 		socket.broadcast.emit('animations', activeAnimations)
 	})
 
-	socket.on('toggle', function(){
+	socket.on('toggle', function () {
 		RUNNING = !RUNNING
 
 		socket.broadcast.emit('status', RUNNING)
@@ -66,24 +67,25 @@ io.sockets.on('connection', function (socket) {
 })
 
 
-function Strip(arr){
+function Strip(arr) {
 	var animations = JSON.stringify(arr.slice())
-		animations = JSON.parse(animations)
+	animations = JSON.parse(animations)
 
 	delete animations.frame
 
 	return animations
 }
 
-function RenderStrip(){
-	for(var i = 0; i < ActiveAnimations.length; i++){
+function RenderStrip() {
+	for (var i = 0; i < ActiveAnimations.length; i++) {
 		Pixels = ActiveAnimations[i].requestFrame(Frame, Pixels)
 	}
 
 	Device.transfer(Pixels.buffer, Pixels.readBuffer)
 
-	if(RUNNING){
+	if (RUNNING) {
 		Frame++
+		console.log(Pixels.buffer);
 		setTimeout(RenderStrip, 1000 / FPS)
 	}
 }
